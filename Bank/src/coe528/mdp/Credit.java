7a2bffd3a4f54aae7f2858f02fec526d585b2e1f
gelarehmollabagheri <gelareh.mollabagheri@ryerson.ca>
2015-12-04 19:41:04 -0500

    Update Credit.java

--- Bank/src/coe528/mdp/Credit.java
+++ Bank/src/coe528/mdp/Credit.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package coe528.mdp;
 
 /**
  *
- * @author Brian
+ * @author Gelareh
  */
 public class Credit {
+      
+    public static int crLimit = 2500;  
+    
+    //This section is about paying up your credit card balance
+    public void pay_balance(int amount) {
+        if (amount > 0 && crLimit < 2500) {
+        Credit.this.crLimit += amount;
+        System.out.println("Your current balance is: " + crLimit);
+        } else {
+            System.err.println("You must deposit funds greater than 0$.");
+        }
+    }
+      
+    //This section is about purchasing with your credit card
+    public void bill_payment(int amount) {
+        if (Credit.this.crLimit >= amount) {
+        Credit.this.crLimit -= amount;
+        System.out.println("Your current balance is: " + crLimit);
+        } else if (amount > crLimit) {
+            System.err.println("Over credit card limit.");
+        }    
+    }
+
+    public static int getCrBal() {
+        return crLimit;
+    }
+
+    public static void setCrBal(int crLimit) {
+        Credit.crLimit = crLimit;
+    }      
         
 }
