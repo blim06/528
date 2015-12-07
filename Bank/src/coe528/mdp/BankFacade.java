78d5ae8fd0959928eed1da87edcba46719c9f1e3
mpieprzy <mpieprzy@ryerson.ca>
2015-12-07 12:04:45 -0500

    Update BankFacade.java

--- Bank/src/coe528/mdp/BankFacade.java
+++ Bank/src/coe528/mdp/BankFacade.java
@@ -2,23 +2,44 @@ package coe528.mdp;
 
 public class BankFacade {
    private ATM atm;
-   private Account acc;
-   private Chequeing cheq;
-   private Saving sav;
-   private Credit credit;
-   private Mortgage mortgage;
+   private OnlineBanking om;
    
 
-     public BankFacade(ATM atm, Account acc, Chequeing cheq, Saving sav, Credit credit, Mortgage mortgage) {
-        this.atm = atm;
-        this.acc = acc;
-        this.cheq = cheq;
-        this.sav = sav;
-        this.credit = credit;
-        this.mortgage = mortgage;
+     public BankFacade() {
+        atm = new ATM();
+        om = new OnlineBanking();
     }
-   
-   
-
-  
-}
+     
+     public void selectAccountOM () {
+        om.selectAccount();
+     }
+     public void selectAccountATM() {
+        atm.selectAccount();
+     }
+     public void menuAccountOM(){
+        om.menuAccount();
+     }
+     public void menuAccountATM(){
+        atm.menuAccount();
+     }
+     public void ChequingMenuOM(){
+        om.ChequingMenu();
+     }
+     public void ChequingMenuATM(){
+        atm.ChequingMenu();
+     }
+     public void SavingMenuOM(){
+        om.SavingMenu();
+     }
+     public void SavingMenuATM(){
+        atm.SavingMenu();
+     }
+     public void CreditMenuOM(){
+        om.CreditMenu();
+     }
+     public void CreditMenuATM(){
+        atm.CreditMenu();
+     }
+     public void MoneyXferOM(){
+        om.MoneyXfer();
+     }
