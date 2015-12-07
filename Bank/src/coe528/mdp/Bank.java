bf15f8a05f26771b9b2c92fe568f65d8005d8537
mpieprzy <mpieprzy@ryerson.ca>
2015-12-07 12:05:31 -0500

    Update Bank.java

--- Bank/src/coe528/mdp/Bank.java
+++ Bank/src/coe528/mdp/Bank.java
@@ -2,12 +2,10 @@ package coe528.mdp;
 
 //This interface holds all of the methods present in the daughter classes.
 public interface Bank {
-    public void action();
     public void selectAccount();
     public void menuAccount();
     public void ChequingMenu();
     public void SavingMenu();
     public void CreditMenu();
-    public void pay_balance();
-    public void bill_payment();
+    public void MoneyXfer();
 }
