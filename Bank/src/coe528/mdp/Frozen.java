cc60f6d11df779ffbec3b61b467b05df082f0fbd
mpieprzy <mpieprzy@ryerson.ca>
2015-12-07 23:17:38 -0500

    Update Frozen.java

--- Bank/src/coe528/mdp/Frozen.java
+++ Bank/src/coe528/mdp/Frozen.java
@@ -1,7 +1,15 @@
 package coe528.mdp;
 
+/**
+ *
+ * @author mpieprzy
+ */
 public class Frozen implements State {
     
+    /**
+     *Sets the state of an object to Frozen.
+     * @param cxt context in which the state is being used for
+     */
     public void checkif(Context cxt) {
       System.out.println("Account is frozen");
       cxt.setState(this);	
