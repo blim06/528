621d5963606200f38d45930dcfa35b1efaeaae65
mpieprzy <mpieprzy@ryerson.ca>
2015-12-07 23:17:08 -0500

    Update GoodStanding.java

--- Bank/src/coe528/mdp/GoodStanding.java
+++ Bank/src/coe528/mdp/GoodStanding.java
@@ -1,7 +1,16 @@
 package coe528.mdp;
 
+/**
+ *
+ * @author mpieprzy
+ */
 public class GoodStanding implements State {
-     public void checkif(Context cxt) {
+
+    /**
+     *Sets the state of an object to GoodStanding.
+     * @param cxt context in which the state is being used for
+     */
+    public void checkif(Context cxt) {
       System.out.println("Account is in good standing");
       cxt.setState(this);	
    }
